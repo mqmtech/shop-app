83668dcd03ddeb4bb471f7a7e980642bdbd9127b
Mario Quesada <centralmqmtech@gmail.com>
2012-07-08 12:56:17 +0200

    added GooglePlus and edited FacebookButton

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,9 +6,11 @@
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (false && !in_array(@$_SERVER['REMOTE_ADDR'], array(
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
+    '192.168.1.6',
+    'cxreloaded.sytes.net',
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
